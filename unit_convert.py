66beccfd881d2a927bdfc4dabf30d8de6692e542
Michal Kowalski <kowal_@outlook.com>
2021-10-01 22:58:26 +0200

    added base-16 convetrion both ways

--- unit_convert.py
+++ unit_convert.py
@@ -1,6 +1,4 @@
-from sys import argv
-
-def to_binary(num:int) -> int:
+def to_base2(num:int) -> int:
     '''
     Converts an Int type base-10 
     number to base-2(8-bits)
@@ -18,7 +16,7 @@ def to_binary(num:int) -> int:
                 result += '0';
         return int(result);
 
-def to_decimal(num:int,base:int = 2) -> int:
+def to_base10(num,base:int = 2) -> int:
     '''
     Converts an Int type of user specified 
     base(deafult base-2) number to base-10
@@ -36,17 +34,15 @@ def to_decimal(num:int,base:int = 2) -> int:
                 bits_count += 1;
         return int(result);
     
-    if base == 8:
-        num = str(num)[::-1];
+    if base == 16 or base == 8:
+        base16 = ['0','1','2','3','4','5','6','7','8','9',"A","B","C","D","E","F"];
+        num = str(num)[::-1].upper();
         result = 0;
         for x, y in enumerate(num):
-            result += int(y)*(10**int(x));
-            print(result,y,x);
+            result += int(base16.index(y))*(base**int(x));
         return int(result);
 
-print(to_decimal(53243,8))
-
-def to_octal(num:int) -> int:
+def to_base8(num:int) -> int:
     '''
     Converts an Int type base-10 
     number to base-8
@@ -56,3 +52,15 @@ def to_octal(num:int) -> int:
         result += str(num%8);
         num = int(num/8);
     return int(result[::-1]);
+
+def to_base16(num:int) -> str:
+    '''
+    Converts an Int type base-10 
+    number to base-16
+    '''
+    base16 = ['0','1','2','3','4','5','6','7','8','9',"A","B","C","D","E","F"];
+    result = '';
+    while num != 0:
+        result += base16[int(num%16)];
+        num = int(num/16);
+    return result[::-1];
