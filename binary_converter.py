88db4b318674da382be34612c68790acf6155ce5
Michal Kowalski <kowal_@outlook.com>
2021-10-01 22:04:59 +0200

    added octal conversion

--- binary_converter.py
+++ binary_converter.py
@@ -1,13 +1,13 @@
 from sys import argv
 
-def to_binary(num: int) -> str:
+def to_binary(num:int) -> int:
     '''
-    This function converts an int in range of 0-255 to an 8bit binary integer
+    Converts an Int type base-10 number to base-2(8-bits)
     '''
     bits = [128,64,32,16,8,4,2,1];
     result = '';
     if (num > sum(bits) or num < 0):
-        return "number to high or low"
+        return "only acceptes values between 0-255"
     else:
         for bit in bits:
             if (num >= bit):
@@ -15,30 +15,31 @@ def to_binary(num: int) -> str:
                 num -= bit;
             else: 
                 result += '0';
-        return result;
+        return int(result);
 
-def to_decimal(num:str) -> int:
+def to_decimal(num:int) -> int:
     '''
-    This function converts an binary Integer in range of 0-255 to a decimal number
+    Converts an Int type base-2 number to base-10
     '''
+
     bits = [128,64,32,16,8,4,2,1];
     bits_count = 0;
     result = 0;
-    for x in num:
+    for x in str(num):
         if int(x) == 1:
             result += bits[bits_count];
             bits_count += 1;
         else:
             result += 0;
             bits_count += 1;
-    return result;
+    return int(result);
 
-try:
-    if (argv[1] == '-I' or argv[1] == "--Info" or argv[1] == "--help" or argv[1] == "" or argv[1] == None):
-        print("INFO\n\nThis is a simple CLI tool to convert Base-10 numer to 8-bit binary and reversed\n\nCommands:\n -D/--Decimal <8-bit binary number>\n -B/--Binary <base-10 number> \n -I/--Info for info page");
-    elif (argv[1] == '-B' or argv[1] == "--Binary"):
-        print(to_binary(int(argv[2])));
-    elif (argv[1] == '-D' or argv[1] == "--Decimal"):
-        print(to_decimal(str(argv[2])));
-except:
-    print("INFO\n\nThis is a simple CLI tool to convert Base-10 numer to 8-bit binary and reversed\n\nCommands:\n -D/--Decimal <8-bit binary number>\n -B/--Binary <base-10 number> \n -I/--Info for info page");
+def to_octal(num:int) -> int:
+    '''
+    Converts an Int type base-10 number to base-8
+    '''
+    result = '';
+    while num != 0:
+        result += str(num%8);
+        num = int(num/8);
+    return int(result[::-1]);
