7416bae4852562630afe0fd64a1690f9643ca444
Michal Kowalski <kowal_@outlook.com>
2021-10-03 19:38:16 +0200

    added an extra function to convert a base-10 to roman

--- unit_convert.py
+++ unit_convert.py
@@ -64,3 +64,19 @@ def to_base16(num:int) -> str:
         result += base16[int(num%16)];
         num = int(num/16);
     return result[::-1];
+
+
+
+#I = 1 V = 5 X = 10 L = 50 C = 100 D = 500 M = 1000 
+def to_roman(num:int) -> str:
+    num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')];
+    roman = '';
+    while num > 0:
+        for i, r in num_map:
+            while num >= i:
+                roman += r;
+                num -= i;
+
+    return str(roman);
+print(to_roman(14))
